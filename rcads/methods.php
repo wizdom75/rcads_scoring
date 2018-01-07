896a210b9d625196e44e7ed78a1397b429cf2094
wizdom75 <pmn@outlook.com>
2018-01-07 19:25:31 +0000

    Update methods.php

--- rcads/methods.php
+++ rcads/methods.php
@@ -5,10 +5,11 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+include('/classes/dbconnect.php');
 
 function cleanInput($input){
     $input = strip_tags($input);
     $input = filter_var( $input, FILTER_SANITIZE_STRING);
-    $input = mysqli_real_escape_string($con, $input);
+    $input = mysqli_real_escape_string($this->dbConnect(), $input);
     return $input;
 }
